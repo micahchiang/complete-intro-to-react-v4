bd1cb33e885e9c0a63513c27822872ad74cb6d09
Micah Chiang <micahkchiang@gmail.com>
2019-06-11 16:57:53 -0700

    redo for latest react tools

--- src/App.js
+++ src/App.js
@@ -1,95 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
-import pf from "petfinder-client";
-import Results from "./Results";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import { Provider } from "./SearchContext";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      location: "Seattle, WA",
-      animal: "",
-      breed: "",
-      breeds: [],
-      handleAnimalChange: this.handleAnimalChange,
-      handleBreedChange: this.handleBreedChange,
-      handleLocationChange: this.handleLocationChange,
-      getBreeds: this.getBreeds
-    };
-  }
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value
-      },
-      this.getBreeds
-    );
-  };
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed
-        .list({ animal: this.state.animal })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({
-              breeds: data.petfinder.breeds.breed
-            });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        })
-        .catch(console.error);
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
-  }
-  render() {
-    return (
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
-        <Provider value={this.state}>
-          <Router>
-            <Results path="/" />
-            <Details path="/details/:id" />
-            <SearchParams path="/search-params" />
-          </Router>
-        </Provider>
-      </div>
-    );
-  }
+const Pet = ({name, animal, breed}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
+    ])
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+    return React.createElement(
+        "div",
+        { id: "something-important"},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Lab-mastiff"}),
+            React.createElement(Pet, { name: "Tubbi", animal: "Dog", breed: "Shiba-Inu"}),
+            React.createElement(Pet, { name: "Rosy", animal: "Dog", breed: "German Shepherd"}),
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
