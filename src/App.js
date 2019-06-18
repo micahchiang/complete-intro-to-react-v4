6f587eb2c8cfaf99f6122e762412f5d0df11073e
Micah Chiang <micahkchiang@gmail.com>
2019-06-18 16:17:07 -0700

    begin hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div id="something-important">
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={e => setLocation(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
